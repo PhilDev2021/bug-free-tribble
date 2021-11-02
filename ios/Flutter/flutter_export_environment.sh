98e4b97399e31bf68ec0dc1a75bdfd331f20c0f0
CodePhilanthropist <barrigaete@gmail.com>
2021-11-02 18:16:27 +0800

    debugging

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rian/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rian/Documents/dicee-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rian/Documents/bug-free-tribble"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/rian/Documents/dicee-flutter/lib/main.dart"
+export "FLUTTER_TARGET=/Users/rian/Documents/bug-free-tribble/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/rian/Documents/dicee-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/rian/Documents/bug-free-tribble/.dart_tool/package_config.json"
